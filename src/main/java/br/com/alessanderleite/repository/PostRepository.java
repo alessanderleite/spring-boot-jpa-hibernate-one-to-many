92930092c32ab2e38fabcbe12c39f35421beba1a
alessanderleite <alessander.logistica@gmail.com>
2019-09-09 17:28:27 -0300

    create the interface named PostRepository

---
+++ src/main/java/br/com/alessanderleite/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package br.com.alessanderleite.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.alessanderleite.model.Post;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long>{
+
+}
