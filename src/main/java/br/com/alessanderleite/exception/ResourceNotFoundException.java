f64424859ac2625d577aa4f304c6605f7461c041
alessanderleite <alessander.logistica@gmail.com>
2019-09-09 17:46:32 -0300

    create the class to handler exception named ResourceNotFoundException

---
+++ src/main/java/br/com/alessanderleite/exception/ResourceNotFoundException.java
@@ -0,0 +1,21 @@
+package br.com.alessanderleite.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException() {
+		super();
+	}
+	
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+	
+	public ResourceNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}	
+}
