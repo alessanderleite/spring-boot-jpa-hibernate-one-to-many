d627aeab7c9c59a12fc38de54bd030959c531f4b
alessanderleite <alessander.logistica@gmail.com>
2019-09-08 23:52:55 -0300

    add the @EnableJpaAuditing annotation inside the main class

--- src/main/java/br/com/alessanderleite/SpringBootJpaHibernateOneToManyApplication.java
+++ src/main/java/br/com/alessanderleite/SpringBootJpaHibernateOneToManyApplication.java
@@ -2,8 +2,10 @@ package br.com.alessanderleite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringBootJpaHibernateOneToManyApplication {
 
 	public static void main(String[] args) {
