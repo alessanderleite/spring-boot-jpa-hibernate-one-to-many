ae2305256c2cf9774ee4b2e326be4cdf0d2ec56c
alessanderleite <alessander.logistica@gmail.com>
2019-09-10 15:54:24 -0300

    change the wrong import to call Pageable object

--- src/main/java/br/com/alessanderleite/repository/CommentRepository.java
+++ src/main/java/br/com/alessanderleite/repository/CommentRepository.java
@@ -1,9 +1,10 @@
 package br.com.alessanderleite.repository;
 
-import java.awt.print.Pageable;
+
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +13,5 @@ import br.com.alessanderleite.model.Comment;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>{
 	Page<Comment> findByPostId(Long postId, Pageable pageable);
-	Optional<Comment> findByIdPostId(Long id, Long postId);
+	Optional<Comment> findByIdAndPostId(Long id, Long postId);
 }
